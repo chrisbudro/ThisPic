77780f3a517b471ede4ea703282e8dd4dc896ffa
Chris Budro <chrisbudro@gmail.com>
2015-08-20 11:02:39 -0700

    Renamed Project

--- ThisPic/ThisPic-Bridging-Header.h
+++ ThisPic/ThisPic-Bridging-Header.h
@@ -4,8 +4,8 @@
 //  Copyright 2011-present Parse Inc. All rights reserved.
 //
 
-#ifndef ParseStarterProject_Bridging_Header_h
-#define ParseStarterProject_Bridging_Header_h
+#ifndef ThisPic_Bridging_Header_h
+#define ThisPic_Bridging_Header_h
 
 // If you are using Facebook, uncomment this line to get automatic import of the header inside your project.
 //#import <ParseFacebookUtils/PFFacebookUtils.h>
